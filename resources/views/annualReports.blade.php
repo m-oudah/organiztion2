de65fed0d7c748b64fb08909a3cf233d6357351b
MOHAMMED OUDAH <34090835+m-oudah@users.noreply.github.com>
2022-09-09 00:07:27 +0300

    second update

--- resources/views/annualReports.blade.php
+++ resources/views/annualReports.blade.php
@@ -6,22 +6,20 @@
 @section('content')
 
 
-
-    <!-- Page Header Start -->
-    <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container text-center">
-            <h1 class="display-4 text-white animated slideInDown mb-4">{{__('trans.Annual Reports')}}</h1>
+     <!-- Page Header Start -->
+ <div class="container-fluid header-bg py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <h1 class="display-4 text-white mb-3 animated slideInDown">{{__('trans.Annual Reports')}}</h1>
             <nav aria-label="breadcrumb animated slideInDown">
-                <ol class="breadcrumb justify-content-center mb-0">
+                <ol class="breadcrumb mb-0">
                     <li class="breadcrumb-item"><a class="text-white" href="{{route('index',$lang)}}">{{__('trans.home')}}</a></li>
                     <li class="breadcrumb-item"></li>
                     <li class="breadcrumb-item text-primary active" aria-current="page">{{__('trans.Annual Reports')}}</li>
                 </ol>
             </nav>
         </div>
-    </div>
-    <!-- Page Header End -->
-
+ </div>
+<!-- Page Header End -->
 
 
 
@@ -39,19 +37,8 @@
                         </div>
                         <h5 class="mb-3">{{__('trans.article title')}}</h5>
                         <p>{{__('trans.subarticle')}}</p>
-
                     </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="{{asset('assets/img/courses-1.jpg')}}" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                {{__('trans.View')}}
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
@@ -65,17 +52,7 @@
                         <p>{{__('trans.subarticle')}}</p>
 
                     </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="{{asset('assets/img/courses-2.jpg')}}" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                {{__('trans.View')}}
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                   
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
@@ -89,17 +66,7 @@
                         <p>{{__('trans.subarticle')}}</p>
 
                     </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="{{asset('assets/img/courses-3.jpg')}}" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                {{__('trans.View')}}
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
@@ -113,17 +80,7 @@
                         <p>{{__('trans.subarticle')}}</p>
 
                     </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="{{asset('assets/img/courses-3.jpg')}}" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                {{__('trans.View')}}
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
@@ -137,17 +94,7 @@
                         <p>{{__('trans.subarticle')}}</p>
 
                     </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="{{asset('assets/img/courses-3.jpg')}}" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                {{__('trans.View')}}
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
@@ -161,23 +108,27 @@
                         <p>{{__('trans.subarticle')}}</p>
 
                     </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="{{asset('assets/img/courses-3.jpg')}}" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                {{__('trans.View')}}
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div
+                    class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
+                    <div class="text-center p-4 pt-0">
+                        <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
+                            <small>15 May 2022</small>
+                        </div>
+                        <h5 class="mb-3">{{__('trans.article title')}}</h5>
+                        <p>{{__('trans.subarticle')}}</p>
+
+                    </div>
+                    
+                </div>
+            </div>
+            
         </div>
 
 
-       
 
 
 
